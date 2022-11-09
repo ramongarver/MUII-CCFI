61238023a98c3c9a1721ef74906f01ef2a2e314c
Ramón García Verjaga <ramongarver2000@gmail.com>
2022-11-09 15:34:05 +0100

    Hito-1: Fix header title

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="py-5">
       <h1 className="mx-auto text-center text-5xl font-black md:w-2/3">
-        <span className="text-red-600">Remem</span>ilm 🍿
+        <span className="text-red-600">Rememb</span>ilm 🍿
       </h1>
     </header>
   );
